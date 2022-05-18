e8aed77c4ef97c1446677c558880a010676269c2
Ivonne <ivonne9410@hotmail.com>
2022-05-18 10:32:56 +0200

    Actualizacion final

--- src/components/TheMenu.vue
+++ src/components/TheMenu.vue
@@ -1,15 +1,18 @@
 <template>
 
     <nav class="main-menu">
-        <router-link to="/">Inicio</router-link> 
-        <router-link to="/experience">Experiencia</router-link> 
-        
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/experience">Experiencia</router-link>
+
     </nav>
+
 
 </template>
 
 <script setup>
 
-    require("@/assets/scss/TheMenu.scss")
+
+
+require("@/assets/scss/TheMenu.scss")
 
 </script>
